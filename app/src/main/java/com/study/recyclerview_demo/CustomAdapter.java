d619b6d7eeecc664a78102cbe02871eabb49ae5d
kjy <aty753@naver.com>
2020-12-02 22:58:13 +0900

    생성된 뷰 홀더에 데이터를 바인딩할 onBindViewHolder 내용 작성

--- app/src/main/java/com/study/recyclerview_demo/CustomAdapter.java
+++ app/src/main/java/com/study/recyclerview_demo/CustomAdapter.java
@@ -1,5 +1,6 @@
 package com.study.recyclerview_demo;
 
+import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -29,19 +30,24 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomView
         this.mList = mList;
     }
 
+    /*카운트 다음으로 호출되는 함수로 ViewHolder이 생성되는 함수로서 여기서 ViewHolder 객체를만들어주면 됩니다.*/
     @NonNull
     @Override
     public CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         return null;
     }
 
+    /*생성된 뷰 홀더에 데이터를 바인드 해주는 함수입니다.*/
     @Override
     public void onBindViewHolder(@NonNull CustomViewHolder holder, int position) {
-
+      holder.id.setText(mList.get(position).getId());
+      holder.english.setText(mList.get(position).getEnglish());
+      holder.korean.setText(mList.get(position).getKorean());
     }
-
+        /*가정 먼저 우리가 뿌려줄 데이터의 전체 길이를 리턴하는 getItemCount 메소드가 실행.*/
     @Override
     public int getItemCount() {
         return 0;
+
     }
 }
