896c80590d08df4fa5141f0049512a3e956e65d7
kjy <aty753@naver.com>
2020-12-02 22:38:13 +0900

    뷰홀더 생성자 생성

--- app/src/main/java/com/study/recyclerview_demo/CustomAdapter.java
+++ app/src/main/java/com/study/recyclerview_demo/CustomAdapter.java
@@ -1,11 +1,15 @@
 package com.study.recyclerview_demo;
 
+import android.view.View;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class CustomAdapter extends RecyclerView.ViewHolder {
 
-    public Class CustomViewHolder extends RecyclerView.ViewHolder{
-
-
-    }
+  public class CustomViewHolder extends RecyclerView.ViewHolder {
+      public CustomViewHolder(@NonNull View itemView) {
+          super(itemView);
+      }
+  }
 }
