dd4fc9f2cd12cad034390dd946f8f486efb074df
kjy <aty753@naver.com>
2020-12-02 21:59:05 +0900

    get&set 작성

--- app/src/main/java/com/study/recyclerview_demo/Dictionary.java
+++ app/src/main/java/com/study/recyclerview_demo/Dictionary.java
@@ -6,5 +6,27 @@ public class Dictionary {
     private String English;
     private String Korean;
 
-    
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEnglish() {
+        return English;
+    }
+
+    public void setEnglish(String english) {
+        English = english;
+    }
+
+    public String getKorean() {
+        return Korean;
+    }
+
+    public void setKorean(String korean) {
+        Korean = korean;
+    }
 }
