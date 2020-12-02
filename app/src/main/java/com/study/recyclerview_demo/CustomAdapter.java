c5bd6289622d79d40e8b081a55aceacfc1e61922
kjy <aty753@naver.com>
2020-12-02 22:41:31 +0900

    뷰홀더 생성자에 itemview 를 통해 ui 연결

--- app/src/main/java/com/study/recyclerview_demo/CustomAdapter.java
+++ app/src/main/java/com/study/recyclerview_demo/CustomAdapter.java
@@ -1,6 +1,7 @@
 package com.study.recyclerview_demo;
 
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -8,8 +9,14 @@ import androidx.recyclerview.widget.RecyclerView;
 public class CustomAdapter extends RecyclerView.ViewHolder {
 
   public class CustomViewHolder extends RecyclerView.ViewHolder {
+      protected TextView id;
+      protected TextView english;
+      protected TextView korean;
       public CustomViewHolder(@NonNull View itemView) {
           super(itemView);
+          this.id = (TextView) itemView.findViewById(R.id.id_listitem);
+          this.english = (TextView) itemView.findViewById(R.id.english_listitem);
+          this.korean = (TextView) itemView.findViewById(R.id.korean_listitem);
       }
   }
 }
