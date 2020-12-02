3de11206123fd44d471e738812f5ca7f5565ae0d
kjy <aty753@naver.com>
2020-12-02 22:42:59 +0900

    mList라는 이름으로 데이터 배열 정의

--- app/src/main/java/com/study/recyclerview_demo/CustomAdapter.java
+++ app/src/main/java/com/study/recyclerview_demo/CustomAdapter.java
@@ -6,7 +6,11 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class CustomAdapter extends RecyclerView.ViewHolder {
+
+    private ArrayList<Dictionary> mList;
 
   public class CustomViewHolder extends RecyclerView.ViewHolder {
       protected TextView id;
