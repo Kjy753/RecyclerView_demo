8a1fd9d22c641e5f0fb1f3891e12f7387810c29a
kjy <aty753@naver.com>
2020-12-02 22:06:39 +0900

    커스텀 어뎁터 뷰홀더 클래스 생성

--- app/src/main/java/com/study/recyclerview_demo/CustomAdapter.java
+++ app/src/main/java/com/study/recyclerview_demo/CustomAdapter.java
@@ -3,4 +3,9 @@ package com.study.recyclerview_demo;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class CustomAdapter extends RecyclerView.ViewHolder {
+
+    public Class CustomViewHolder extends RecyclerView.ViewHolder{
+
+
+    }
 }
