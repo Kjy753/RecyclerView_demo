7e55f75d33dade077198a0a791a596e28a56d63b
kjy <aty753@naver.com>
2020-12-02 23:09:33 +0900

    뷰가 나열되는 형태를 관리하기 위해 레이아웃 매니저를 작성

--- app/src/main/java/com/study/recyclerview_demo/MainActivity.java
+++ app/src/main/java/com/study/recyclerview_demo/MainActivity.java
@@ -1,14 +1,28 @@
 package com.study.recyclerview_demo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Dictionary> mArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview_main_list);
+        LinearLayoutManager mLinearLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLinearLayoutManager);
+
+
+
+
     }
 }
